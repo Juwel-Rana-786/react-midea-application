425708f7f7a56cfd2d76cef9eb1fd8047bf9b6e1
Juwel <developerjuwel792@gmail.com>
2023-11-04 01:13:11 +0600

    project done

--- src/Components/Media.jsx
+++ src/Components/Media.jsx
@@ -1,48 +1,156 @@
-import { useState } from "react"
-import styled from "styled-components"
-import { data } from '../../db'
+import { useEffect, useRef, useState } from "react";
+import { RiImageAddFill } from "react-icons/ri";
+import styled from "styled-components";
+import { v4 as uuidv4 } from 'uuid';
 
+const data = [
+    {
+        "id": 1,
+        "productImage": "https://i.postimg.cc/8PRkpn9f/image-11.jpg"
+    },
+    {
+        "id": 2,
+        "productImage": "https://i.postimg.cc/HLskP4Sc/image-2.webp"
+    },
+    {
+        "id": 3,
+        "productImage": "https://i.postimg.cc/Cx01bVk4/image-3.webp"
+    },
+    {
+        "id": 4,
+        "productImage": "https://i.postimg.cc/qRNMpBHp/image-4.webp"
+    },
+    {
+        "id": 5,
+        "productImage": "https://i.postimg.cc/TP738LSk/image-5.webp"
+    },
+    {
+        "id": 6,
+        "productImage": "https://i.postimg.cc/RVKh306F/image-6.webp"
+    },
+    {
+        "id": 7,
+        "productImage": "https://i.postimg.cc/0Qh5jg9D/image-7.webp"
+    },
+    {
+        "id": 8,
+        "productImage": "https://i.postimg.cc/FzcrJPKq/image-8.webp"
+    },
+    {
+        "id": 9,
+        "productImage": "https://i.postimg.cc/MZjK4GYD/image-9.webp"
+    },
+    {
+        "id": 10,
+        "productImage": "https://i.postimg.cc/hPWPqqyq/image-10.jpg"
+    },
+    {
+        "id": 11,
+        "productImage": "https://i.postimg.cc/nh1Lkw1f/image-1.webp"
+    },
 
+];
 const Media = () => {
 
-    const [imgData, setImgData] = useState(data)
-    const [selectImg, setSelectImg] = useState([])
+    const [imgData, setImgData] = useState([])
+    const [isChecked, setIsChecked] = useState([])
+    const inputRef = useRef()
+
+
+
+    useEffect(() => {
+
+        // const api = "http://localhost:3000/image"
+        // try {
+        //     axios.get(api)
+        //         .then((res) => setImgData(res.data))
+        // } catch (error) {
+        //     console.log("object")
+        // }
+        setImgData(data)
+
+    }, [])
 
     const handleChange = (e) => {
-        const { name, checked } = e.target;
-        if (name === "allSelect") {
-            const checkedValue = data.map((value) => { return { ...value, isChecked: checked } });
-            setImgData(checkedValue);
-            (checked) ? setSelectImg(checkedValue) : setSelectImg([])
+        const { value, checked } = e.target;
+        if (checked) {
+            setIsChecked([...isChecked, value])
 
 
         } else {
-            return;
+            setIsChecked(isChecked.filter((curEle) => curEle !== value))
+
+
         }
+
+    }
+
+    const handleInputImgClick = () => {
+        inputRef.current.click();
+    }
+    const uploadImg = (e) => {
+
+        const newItem = {
+            id: uuidv4(),
+            productImage: URL.createObjectURL(e.target.files[0])
+        }
+
+
+        setImgData([...imgData, newItem])
+
+    }
+
+
+    const Delete = () => {
+        const conf = window.confirm('Are you sure you want to delete')
+        if (conf) {
+            var filteredArray = imgData.filter(function (n) {
+                return isChecked.indexOf(String(n.id)) == -1;
+            });
+            setImgData(filteredArray);
+            setIsChecked([])
+        }
+
+
+
     }
     return (
         <Wrapper >
-            <div className="header d-flex justify-content-between px-5 py-3 fw-bold ">
-                <input type="checkbox" name="allSelect" checked={!imgData.some((data) => data?.isChecked !== true)} onChange={handleChange} id="" />
-                <span> {selectImg.length} Files Selected</span>
-                <span>Delete Files</span>
+            <div className="header d-flex justify-content-between pe-5 py-3 fw-bold ">
+                <div>
+
+                    {(isChecked.length > 0) && <input type="checkbox"
+                        value="allSelect"
+                        checked={(isChecked.length > 0) && "true"}
+
+
+                        id="" />}
+
+                    {(isChecked.length > 0) && <span> {isChecked.length} Files Selected</span>}
+
+                </div>
+                <button className="delete btn fw-bold p-0 text-danger" onClick={Delete}>Delete Files</button>
             </div>
             <div className="grid-container py-5">
                 {imgData.map((data, i) => {
-
                     return (
 
-                        <div className={`item item${i} h-100 w-100 position-relative`} key={i} >
-                            <input className="checkBox" type="checkbox" checked={data?.isChecked || false} name={data.id} onChange={handleChange} id={`item${i}`} />
+                        <div key={i} className={`item item${i} h-100 w-100 position-relative`} >
+                            <input className="checkBox" type="checkbox" value={data.id} onChange={handleChange} id={`item${i}`} />
                             <label className="overlay h-100 w-100" htmlFor={`item${i}`}>
-                                <img className=" img-fluid" src={data.productImage} alt="product-img" />
+                                <img loading="lazy" className=" img-fluid" src={data.productImage} alt="product-img" />
                             </label>
-
                         </div>
                     )
                 })}
-                <div className="upload-image">
-                    <img src="" alt="" />
+                <div onClick={handleInputImgClick} className="upload-image d-flex justify-content-center align-items-center  ">
+                    <div>
+                        <span className="icon text-center"><RiImageAddFill /></span>
+                        <div> Upload Image</div>
+                        <input ref={inputRef} className="d-none" onChange={uploadImg} type="file" />
+                    </div>
+
+
                 </div>
             </div>
 
@@ -50,13 +158,15 @@ const Media = () => {
     )
 }
 const Wrapper = styled.div`
-width: 80vw;
 
+width: 90vw;
+.header{
+    border-bottom: 1px solid #ccc;
+}
 
 .grid-container{
     display: grid;
-    grid-template-columns:repeat(auto-fit,250px);
-
+    grid-template-columns:repeat(auto-fit,minmax(250px,1fr));
     grid-gap: 20px;
 }
 .item{
@@ -104,7 +214,15 @@ img{
    
 }
 
-
+.upload-image{
+    border: 1px solid #f4f4f4;
+    cursor: pointer;
+    span{
+      font-size: 60px;  
+      display: block;
+    }
+    
+}
 
 
 `
