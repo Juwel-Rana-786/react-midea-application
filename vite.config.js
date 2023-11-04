a6d64601ff7bf1dfb758b8019cfbb056b6a825a8
Juwel <developerjuwel792@gmail.com>
2023-11-04 16:04:18 +0600

    git- pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: " /react-midea-application/",
   plugins: [react()],
-  base: '/react-midea-application/'
+
 })
